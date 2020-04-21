f2faf1c8acaa5fa80f480c7f2e38a0aac14bc685
ablades <audaris@gmail.com>
2020-04-21 10:21:13 -0700

    add title js

---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Title() {
+    return (
+        <div>
+            <h1>SFPOPOS</h1>
+        </div>
+    )
+}
+
+// Default used for most important export.
+export default Title
