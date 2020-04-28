e929bbac529ddd1386b76a0361ea0963179b309f
ablades <audaris@gmail.com>
2020-04-27 23:27:00 -0700

    complete poposlist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Title from './Title.js';
+import POPOSlist from './POPOSlist.js';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <POPOSlist />
     </div>
   );
 }
---
+++ src/POPOSlist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function POPOSList() {
+    return (
+        <div>
+            <h1>Content</h1>
+        </div>
+    )
+}
+
+export default POPOSList;
