416850fc47f79e1bd07395b922062eb55ce9d872
ablades <audaris@gmail.com>
2020-04-28 09:41:55 -0700

    create footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import Title from './Title.js';
 import POPOSlist from './POPOSlist.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Title />
       <POPOSlist />
+      <Footer />
     </div>
+
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+            <span className="Footer">Audi Blades copyright 2020</span>
+    )
+}
+
+export default Footer;
